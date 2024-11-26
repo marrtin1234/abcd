23966e8b11bd55922867cdb89f634c8c696f3ff0
marrtin1234 <159326220+marrtin1234@users.noreply.github.com>
2024-11-26 13:39:36 +0100

    Add files via upload

---
+++ games.js
@@ -0,0 +1,8 @@
+const games = [
+    { name: "1v1.lol", url: "https://1v1.lol" },
+    { name: "Skribbl.io", url: "https://skribbl.io" },
+    { name: "Gartic.io", url: "https://gartic.io" },
+    { name: "Ev.io", url: "https://ev.io" },
+    { name: "Krunker.io", url: "https://krunker.io" },
+    { name: "Play-CS.com", url: "https://play-cs.com/en/" }
+];
